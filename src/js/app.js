e34386eb75fa34c4f0a679cd4e0e49b7c49c38b5
Nikita Golovchenko <sunburst1715@gmail.com>
2019-09-14 02:47:40 +0300

    add adaptive header,intro and js for them

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,63 @@ import SayHello from './components/sayhello';
 
 $(document).ready(function() {
 
-    new SayHello;
+    const $burger = $('#burger');
+    const $nav = $('#nav');
+    const $header = $('#header');
+    const $scrollDown = $('#scroll-down');
+    
+    // add header background-color
+    function addHeaderBackground() {
+        const scrollValue = $(document).scrollTop();
+        const headerHeight = $header.innerHeight();
+        
+        if(scrollValue > headerHeight) {
+            $header.addClass('background');
+        } else {
+            $header.removeClass('background');
+        }
+    }
 
-    $(document).on('mousemove', function(e) {
-        console.log(e.pageX, e.pageY);
+    // burger click
+    $burger.on('click', function(event) {
+        event.preventDefault();
+
+        $burger.toggleClass('active');
+        $nav.toggleClass('active');
+        
+        if($(this).hasClass('active')) {
+            $header.addClass('background');
+
+        } else {
+            addHeaderBackground();
+        }
+    });
+
+    // scrollDown click
+    $scrollDown.on('click', function(event) {
+        event.preventDefault();
+
+        const windowHeight = $(window).innerHeight();
+        const documentScroll = $(document).scrollTop();
+
+        $('html, body').animate({
+			scrollTop: windowHeight
+		}, 500);
+
+
+    })
+
+    // document scroll
+    $(document).on('scroll', function(event) {
+        addHeaderBackground();
+        $burger.removeClass('active');
+        $nav.removeClass('active');
+    });
+    
+    // resize handler
+    $(window).on('load resize orientationchange', function () {
+        $burger.removeClass('active');
+        $nav.removeClass('active');
     });
 
 });
